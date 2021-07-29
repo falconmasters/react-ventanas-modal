776fae709f12672b3faae87e62b916b2539710a6
Carlos <carlos@falconmasters.com>
2021-07-29 14:59:24 -0500

    Archivos finales

--- src/App.js
+++ src/App.js
@@ -1,13 +1,85 @@
-import React from 'react';
-import './index.css';
+import React, {useState} from 'react';
+import Modal from './componentes/Modal';
 import styled from 'styled-components';
 
 const App = () => {
+	const [estadoModal1, cambiarEstadoModal1] = useState(false);
+	const [estadoModal2, cambiarEstadoModal2] = useState(false);
+	const [estadoModal3, cambiarEstadoModal3] = useState(false);
+	const [estadoModal4, cambiarEstadoModal4] = useState(false);
+
 	return (
 		<div>
 			<ContenedorBotones>
-				<Boton>Modal 1</Boton>
+				<Boton onClick={() => cambiarEstadoModal1(!estadoModal1)}>Modal 1</Boton>
+				<Boton onClick={() => cambiarEstadoModal2(!estadoModal2)}>Modal 2</Boton>
+				<Boton onClick={() => cambiarEstadoModal3(!estadoModal3)}>Modal 3</Boton>
+				<Boton onClick={() => cambiarEstadoModal4(!estadoModal4)}>Modal 4</Boton>
 			</ContenedorBotones>
+
+			{/* Modal #1 */}
+			<Modal
+				estado={estadoModal1}
+				cambiarEstado={cambiarEstadoModal1}
+				titulo="Hola!"
+				mostrarHeader={true}
+				mostrarOverlay={true}
+				posicionModal={'center'}
+				padding={'20px'}
+			>
+				<Contenido>
+					<h1>Ventana Modal</h1>
+					<p>Reutilizable y con opciones de personalización.</p>
+					<Boton onClick={() => cambiarEstadoModal1(!estadoModal1)}>Aceptar</Boton>
+				</Contenido>
+			</Modal>
+
+			{/* Modal #2 */}
+			<Modal
+				estado={estadoModal2}
+				cambiarEstado={cambiarEstadoModal2}
+				titulo="Ventana 2!"
+				mostrarHeader={false}
+				mostrarOverlay={false}
+				posicionModal={'end'}
+				padding={'20px'}
+			>
+				<Contenido>
+					<h1>Ventana Modal 2</h1>
+					<p>Reutilizable y con opciones de personalización.</p>
+					<Boton onClick={() => cambiarEstadoModal1(!estadoModal1)}>Aceptar</Boton>
+				</Contenido>
+			</Modal>
+
+			{/* Modal #3 */}
+			<Modal
+				estado={estadoModal3}
+				cambiarEstado={cambiarEstadoModal3}
+				titulo="Ventana 3!"
+				mostrarHeader={true}
+				mostrarOverlay={false}
+				posicionModal={'start'}
+				padding={'20px'}
+			>
+				<Contenido>
+					<img src="https://images.unsplash.com/photo-1598978510474-09d06dc6a5d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80" alt="" />
+				</Contenido>
+			</Modal>
+
+			{/* Modal #4 */}
+			<Modal
+				estado={estadoModal4}
+				cambiarEstado={cambiarEstadoModal4}
+				titulo="Ventana 4!"
+				mostrarHeader={false}
+				mostrarOverlay={true}
+				posicionModal={'center'}
+				padding={'0px'}
+			>
+				<Contenido>
+					<img src="https://images.unsplash.com/photo-1598978510474-09d06dc6a5d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80" alt="" />
+				</Contenido>
+			</Modal>
 		</div>
 	);
 }
